d734dae511ce2820756f09135628b61e19b994ac
Lil-Echoes <sungbeen.kim@gmail.com>
2022-07-01 09:47:00 +0900

    220701 chapter3 exercises

---
+++ Java/src/Ch03/Exercise1.java
@@ -0,0 +1,19 @@
+package Ch03;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		
+		int sum=0;
+		
+		for (int i=1; i<=20; i++) {
+			if (i % 2 != 0 && i % 3 != 0) {
+				sum += i;
+				i++;
+			}
+		}
+				System.out.printf("2또는 3의 배수를 제외하고 20까지의 합계는 %d이다",sum);
+
+	}
+
+}
---
+++ Java/src/Ch03/Exercise2.java
@@ -0,0 +1,19 @@
+package Ch03;
+
+public class Exercise2 {
+
+	public static void main(String[] args) {
+		
+		int sum = 0, totalsum = 0;
+		
+		for (int i = 1; i <=10; i++) {
+				sum += i;
+				System.out.println(i + "까지 합계 : " +sum);
+				totalsum +=sum;
+		}
+		System.out.println("합계 : " + sum);
+		System.out.println("누계 : " + totalsum);
+
+	}
+
+}
